07178bfb200776456244e406087680790d10be30
lench <leonidgneusev@gamil.com>
2023-10-24 11:41:54 +0500

    .

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 import './data.js';
+import { PHOTOS_COUNT, createImage } from './data.js';
+import { createPictures } from './pictures.js';
+
+createPictures(Array.from( {length: PHOTOS_COUNT}, createImage));
+
