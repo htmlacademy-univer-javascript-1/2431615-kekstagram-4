b83c870eee75bb370f5863b4031d03bda199e942
lench <leonidgneusev@gamil.com>
2023-10-27 02:15:07 +0500

    йоу

--- js/pictures.js
+++ js/pictures.js
@@ -5,25 +5,25 @@ const picturesTemplate = document.querySelector('#picture')
 
 const createPicture = (picture) => {
   const currentPicture = picturesTemplate.cloneNode(true);
+
   currentPicture.querySelector('img').src = picture.url;
   currentPicture.querySelector('img').alt = picture.description;
   currentPicture.querySelector('.picture__comments').textContent = picture.comments.length;
   currentPicture.querySelector('.picture__likes').textContent = picture.likes;
 
-  pictureFragments.append(currentPicture);
+  currentPicture.dataset.id = picture.id;
 
+  pictureFragments.append(currentPicture);
 };
 
 const createPictures = (pictures) => {
 
   const pictureContainer = document.querySelector('.pictures');
-
   pictures.forEach((picture) => {
     createPicture(picture);
   });
 
   pictureContainer.append(pictureFragments);
-
 };
 
 export {createPictures};
